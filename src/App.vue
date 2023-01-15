0d341c770b1578b6f97762d3125604466c6a50f9
Charles Ong <charlesong173@gmail.com>
2023-01-15 16:44:19 +0800

    Installed Ant Design

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+    <a-menu
+      id="dddddd"
+      v-model:openKeys="openKeys"
+      v-model:selectedKeys="selectedKeys"
+      style="width: 256px"
+      mode="inline"
+      @click="handleClick"
+    >
+      <a-sub-menu key="sub1" @titleClick="titleClick">
+        <template #icon>
+          <MailOutlined />
+        </template>
+        <template #title>Navigation One</template>
+        <a-menu-item-group key="g1">
+          <template #icon>
+            <QqOutlined />
+          </template>
+          <template #title>Item 1</template>
+          <a-menu-item key="1">Option 1</a-menu-item>
+          <a-menu-item key="2">Option 2</a-menu-item>
+        </a-menu-item-group>
+        <a-menu-item-group key="g2" title="Item 2">
+          <a-menu-item key="3">Option 3</a-menu-item>
+          <a-menu-item key="4">Option 4</a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+      <a-sub-menu key="sub2" @titleClick="titleClick">
+        <template #icon>
+          <AppstoreOutlined />
+        </template>
+        <template #title>Navigation Two</template>
+        <a-menu-item key="5">Option 5</a-menu-item>
+        <a-menu-item key="6">Option 6</a-menu-item>
+        <a-sub-menu key="sub3" title="Submenu">
+          <a-menu-item key="7">Option 7</a-menu-item>
+          <a-menu-item key="8">Option 8</a-menu-item>
+        </a-sub-menu>
+      </a-sub-menu>
+      <a-sub-menu key="sub4">
+        <template #icon>
+          <SettingOutlined />
+        </template>
+        <template #title>Navigation Three</template>
+        <a-menu-item key="9">Option 9</a-menu-item>
+        <a-menu-item key="10">Option 10</a-menu-item>
+        <a-menu-item key="11">Option 11</a-menu-item>
+        <a-menu-item key="12">Option 12</a-menu-item>
+      </a-sub-menu>
+    </a-menu>
+  </template>
+  <script lang="ts">
+  import { defineComponent, ref, watch } from 'vue';
+  import { MailOutlined, QqOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+  import type { MenuProps } from 'ant-design-vue';
+  export default defineComponent({
+    components: {
+      MailOutlined,
+      QqOutlined,
+      AppstoreOutlined,
+      SettingOutlined,
+    },
+    setup() {
+      const selectedKeys = ref<string[]>(['1']);
+      const openKeys = ref<string[]>(['sub1']);
+      const handleClick: MenuProps['onClick'] = e => {
+        console.log('click', e);
+      };
+      const titleClick = (e: Event) => {
+        console.log('titleClick', e);
+      };
+      watch(
+        () => openKeys,
+        val => {
+          console.log('openKeys', val);
+        },
+      );
+      return {
+        selectedKeys,
+        openKeys,
+  
+        handleClick,
+        titleClick,
+      };
+    },
+  });
+  </script>
+  
+  
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+// import './assets/main.css'
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
 
 createApp(App).mount('#app')
